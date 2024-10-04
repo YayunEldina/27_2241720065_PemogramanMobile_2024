61fe876adb4e52795145fc00c840295049c65a77
Yayun Eldina <yayuneldina02@gmail.com>
2024-10-04 23:22:03 +0700

    Tugas Praktikum 1

--- Pertemuan 6/LaporanTugas_1.md
+++ Pertemuan 6/LaporanTugas_1.md
@@ -7,6 +7,8 @@
 ## **Praktikum 1: Buat Project Baru**
 Membuat project baru dengan nama basic_layout_flutter.
 
+ <img src="img/langkah 1.png">
+
 ## **Langkah 2: Lakukan Slicing sesuai dengan instruksi pada gambar** 
 
  <img src="img/langkah 2.png">
@@ -14,13 +16,32 @@ Membuat project baru dengan nama basic_layout_flutter.
 ## **Langkah 3: Membuat Layout**
 Membentuk layout sesuai dengan gambar.
 
+ <img src="img/langkah 3.png">
+
 ## **Langkah 4: Isi konten pada Column petama**
 Membuat custom Widget untuk menampilkan konten pada Column pertama.
 
+ <img src="img/langkah 4.png">
+
 Isi konten pada Column pertama.
+
+ <img src="img/Langkah 4..png">
+
+Hasilnya:
+
+ <img src="img/langkah 4,.png">
+
 
 ## **Langkah 5: Isi konten pada Column kedua**
 Menyiapkan asset gambar yang akan digunakan.
 
+ <img src="img/langkah 5.png">
+
 Memasukkan asset gambar ke dalam folder assets. dan load gambar ke dalam aplikasi.
+
+<img src="img/langkah 5,.png">
+
+<img src="img/langkah 5..png">
+
+
 
--- Pertemuan 6/basic_layout_flutter/lib/main.dart
+++ Pertemuan 6/basic_layout_flutter/lib/main.dart
@@ -1,125 +1,154 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  MainApp({super.key});
 
-  // This widget is the root of your application.
+  final Icon starStyle = Icon(
+    Icons.star,
+    size: 10,
+    color: Colors.grey[700],
+  );
+
+  final iconSize = 15.0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Column(
+                    children: [
+                      const MyContainer(
+                        child: Text("Strawberry Pavlova"),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const MyContainer(
+                        child: Text(
+                          "Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova."
+                          "Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.",
+                          softWrap: true,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      MyContainer(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              children: [
+                                for (int i = 0; i < 5; i++) starStyle,
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            const Text(
+                              "170 Reviews",
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      MyContainer(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                children: [
+                                  Icon(
+                                    Icons.kitchen_outlined,
+                                    color: Colors.green[300],
+                                    size: iconSize,
+                                  ),
+                                  const Text("PREP:"),
+                                  const Text("25 min"),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Icon(
+                                    Icons.timer_outlined,
+                                    color: Colors.green[300],
+                                    size: iconSize,
+                                  ),
+                                  const Text("COOK:"),
+                                  const Text("1 hr"),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Icon(
+                                    Icons.restaurant_outlined,
+                                    color: Colors.green[300],
+                                    size: iconSize,
+                                  ),
+                                  const Text("FEEDS:"),
+                                  const Text("4 - 6"),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(children: [
+                  Image.asset(
+                    "images/strawberry_pavlova.png",
+                    fit: BoxFit.cover,
+                  ),
+                ]),
+              )
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyContainer extends StatelessWidget {
+  final Widget child;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const MyContainer({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Container(
+      width: double.infinity,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.blue[100],
+        border: Border.all(
+          color: Colors.black,
+          width: 1,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: child,
     );
   }
 }
